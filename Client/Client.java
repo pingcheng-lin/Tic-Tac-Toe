7ea2e72991e96bca8fef63b9f400dfd6b3097d68
Ben <pcbenlin@gmail.com>
2021-06-03 20:00:40 +0800

    add most of objects for gui

--- Client/Client.java
+++ Client/Client.java
@@ -1,23 +1,28 @@
-
 import java.net.*;  
-import java.io.*;  
-class Client{  
-    public static void main(String args[])throws Exception {  
-        Socket s=new Socket("localhost",1234);  
-        DataInputStream din=new DataInputStream(s.getInputStream());  
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+import java.io.*;
+
+public class Client{
+    public static void main(String args[]) throws Exception{
+        //Socket socketClient = new Socket("localhost",1234); //create a client socket  
         
-        String str="",str2="";  
-        while(!str.equals("stop")) {  
-            str=br.readLine();  
-            dout.writeUTF(str);  
-            dout.flush();  
-            str2=din.readUTF();  
-            System.out.println("Server says: "+str2);  
-        }  
+        MyFrame frame = new MyFrame(); //create a frame
         
-        dout.close();  
-        s.close();  
+        /*DataInputStream input=new DataInputStream(socketClient.getInputStream());  
+        DataOutputStream output=new DataOutputStream(socketClient.getOutputStream());  
+        BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));  
+        
+        String str = "", str2 = "";  
+        //while(!str.equals("stop")) {  
+            System.out.println("Player name: ");
+            str = buf.readLine();  
+            output.writeUTF(str);  
+            output.flush();  
+            //str2 = input.readUTF();  
+            //System.out.println("Server says: " + str2);  
+        //}
+        input.close();
+        output.close();  
+        socketClient.close();  */
     }
-}  
+}
+
--- Server/Server.java
+++ Server/Server.java
@@ -1,24 +1,70 @@
-import java.net.*;  
+import java.net.*;
+import java.util.Vector;
 import java.io.*;  
 
-class Server{  
-    public static void main(String args[])throws Exception {  
-        ServerSocket ss=new ServerSocket(1234);  
-        Socket s=ss.accept();  
-        DataInputStream din=new DataInputStream(s.getInputStream());  
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-        
-        String str="",str2="";  
-        while(!str.equals("stop")) {  
-            str=din.readUTF();  
-            System.out.println("client says: "+str);  
-            str2=br.readLine();  
-            dout.writeUTF(str2);  
-            dout.flush();  
-        }  
-        din.close();  
-        s.close();  
-        ss.close();  
+class Server{
+
+    static Vector<String> players = new Vector<String>();
+    static int [][] paper = new int [3][3];
+    public static void main(String args[])throws Exception {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234); //create a server socket
+            while(true) {
+                System.out.println("Waiting...");
+                Socket connection = serverSocket.accept(); //establish connection and waits for the client
+                System.out.println("Success Connect");
+                WaitingRoom wr = new WaitingRoom(connection);
+                Thread th = new Thread(wr);
+                th.start();
+            }
+        } catch(Exception e) {
+            System.out.println(e);
         }
+        
+
+        //serverSocket.close();
+    }
+
+
 }
+
+class WaitingRoom implements Runnable {
+    private Socket connection;
+    private DataInputStream input;
+    private DataOutputStream output;
+    
+    public WaitingRoom(Socket connection) {
+            this.connection = connection;
+    }
+
+    public void run() {
+        try {
+            input = new DataInputStream(connection.getInputStream());  
+            output = new DataOutputStream(connection.getOutputStream());
+            
+            String str="";
+            System.out.println("Wait player name...");
+            str=input.readUTF();
+            Server.players.addElement(str);
+    
+            System.out.println("Player name: " + str);
+            System.out.flush();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        
+
+        //str2=br.readLine();  
+        //output.writeUTF(str2);  
+        //output.flush();  
+        try {
+            input.close();
+            output.close();
+            connection.close(); 
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
